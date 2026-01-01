6d3316501f2b3a9378c3f3d103108f5331a6fded
Kwasi B <boatengkwasi0303@gmail.com>
2026-01-01 09:23:40 +0000

    fix: oidc

--- Infrastructure/azure/environments/staging/backend.tf
+++ Infrastructure/azure/environments/staging/backend.tf
@@ -6,7 +6,7 @@ terraform {
   backend "azurerm" {
     # These values are provided via -backend-config in CI/CD
     # resource_group_name  = "tfstate-rg"
-    # storage_account_name = "tfstateXXXXX"
+    # storage_account_name = "tfstate6bc89fe2"
     # container_name       = "tfstate"
     # key                  = "staging/terraform.tfstate"
     
